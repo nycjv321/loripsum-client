39ea730211c4fcef12aedd7a5473556ba6746de8
Javier L. Velasquez <nycjv321@gmail.com>
2015-10-17 21:08:17 -0400

    init commit

---
+++ spec/loripsum_wrapper_spec.rb
@@ -0,0 +1,61 @@
+require_relative 'spec_helper'
+describe Loripsum::Client do
+  describe '.generate' do
+    it 'gets lorem ipsum with a specified number of paragraphs' do
+      ARGV = %w(-n 4)
+      html_doc = Nokogiri::HTML(Loripsum::Client.generate)
+      expect(html_doc.css('p').size).to eql 4
+    end
+    it 'gets lorem ipsum with a specified number of words' do
+      ARGV = %w(-n 1 -l short)
+      html_doc = Nokogiri::HTML(Loripsum::Client.generate)
+      expect(html_doc.css('p').text.split(" ").size).to be < 50
+    end
+    it 'gets lorem ipsum as plain text' do
+      ARGV = %w(-t)
+      expect(Loripsum::Client.generate).to_not include('<p>')
+    end
+    it 'gets lorem ipsum with a specified length' do
+      ARGV = %w(-c)
+      html_doc = Nokogiri::HTML(Loripsum::Client.generate)
+      expect(html_doc.css('pre').size).to be > 0
+    end
+    it 'gets lorem ipsum with an ul' do
+      ARGV = %w(-u)
+      html_doc = Nokogiri::HTML(Loripsum::Client.generate)
+      expect(html_doc.css('ul > li').size).to be > 0
+    end
+    it 'gets lorem ipsum with an ol' do
+      ARGV = %w(-o)
+      html_doc = Nokogiri::HTML(Loripsum::Client.generate)
+      expect(html_doc.css('ol > li').size).to be > 0
+    end
+    it 'gets lorem ipsum with a description list' do
+      ARGV = %w(-d)
+      html_doc = Nokogiri::HTML(Loripsum::Client.generate)
+      expect(html_doc.css('dl > dd').size).to be > 0
+    end
+    it 'gets lorem ipsum with a block quote' do
+      ARGV = %w(-b)
+      html_doc = Nokogiri::HTML(Loripsum::Client.generate)
+      expect(html_doc.css('blockquote').size).to be > 0
+    end
+    it 'gets lorem ipsum with a specified length' do
+      ARGV = %w(-h)
+      output = Loripsum::Client.generate
+      expect(output[:header]).to_not be_empty
+      expect(output[:body]).to_not be_empty
+    end
+  end
+end
+
+describe Loripsum::OptionsParser do
+  describe '.parse!' do
+    it 'checks for valid length arguments' do
+      ARGV = []
+      ARGV << '-l'
+      ARGV << 'invalid'
+      expect{Loripsum::OptionsParser.parse!}.to raise_error(RuntimeError)
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+
+require_relative '../lib/loripsum'
+require 'rspec'
+require 'nokogiri'
